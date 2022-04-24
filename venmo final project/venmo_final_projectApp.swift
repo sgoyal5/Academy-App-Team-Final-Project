76f58e4308a0e19732da2ab58c00f8b87c890af5
Sana Goyal <sgoyal@unc.edu>
2022-04-24 01:27:05 -0400

    Initial Commit

---
+++ venmo final project/venmo_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  venmo_final_projectApp.swift
+//  venmo final project
+//
+//  Created by Sana Goyal on 4/24/22.
+//
+
+import SwiftUI
+
+@main
+struct venmo_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
